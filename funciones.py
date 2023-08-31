ab7666d9637ef98bd77084907330dfd04f37ddb0
Jorge Andres Allende Medrano     (LATAM) <4615018@cl.lan.com>
2023-08-31 01:26:34 -0400

    análisis de variables con respecto a delay_15

--- funciones.py
+++ funciones.py
@@ -1,7 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def countplot(df,variables,xi,yi):
+def countplot(df,variables,xi,yi,hue=None):
     
     """
     Permite agregar diferentes variables para graficar countplot.
@@ -21,13 +21,39 @@ def countplot(df,variables,xi,yi):
     fig, axs = plt.subplots(sbplots_x_col, sbplots_x_row, figsize=(xi,yi))
     axs = axs.ravel() 
     for i, var in enumerate(variables):
-        sns.countplot(data=df, x=var, ax=axs[i])
+        sns.countplot(data=df, x=var, hue=hue, ax=axs[i])
         axs[i].set_title(f'Countplot de {var}')
         axs[i].set_xticklabels(axs[i].get_xticklabels(), rotation=90)  # Girar las etiquetas del eje x
 
     plt.tight_layout()
     return plt.show()
+
+def histplot(df,variables,xi,yi):
     
+    """
+    Permite agregar diferentes variables para graficar countplot.
+
+    Parámetros:
+    df: dataframe donde se encuentras las variables.
+    variables: Lista de variables del dataframe para graficar.
+    xi: Ancho de los graficos
+    yi: Largo de los graficos
+
+    Retorna:
+    Graficos en dos columnas diferentes.
+    """
+    sbplots_x_row = 2
+    sbplots_x_col = (len(variables) + 1) // 2
+
+    fig, axs = plt.subplots(sbplots_x_col, sbplots_x_row, figsize=(xi,yi))
+    axs = axs.ravel() 
+    for i, var in enumerate(variables):
+        sns.histplot(data=df, x=var, ax=axs[i])
+        axs[i].set_title(f'Countplot de {var}')
+        axs[i].set_xticklabels(axs[i].get_xticklabels(), rotation=90)  # Girar las etiquetas del eje x
+
+    plt.tight_layout()
+    return plt.show()    
 
     
 def distribucion_porcentual(df,cols):
@@ -69,5 +95,21 @@ def validador_high_season(fecha):
     
     return 0
 
+def boxplot(var_bx):
+    """
+    Grafico Boxplot
+
+    Parametros:
+    var_bx = Variable a graficar
+
+    Retorna: 
+    Grafico de boxplot a variable sele
+    """
+    sns.boxplot(data=var_bx)
+
+    plt.title('Boxplot min_diff')
+    plt.ylabel('Minutos')
+
+    plt.show()
 
     
